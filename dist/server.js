9f44612937fa95af5c8731f6de0bebd98c9e2733
Olivier Riccini <olivier-webdev@hotmail.com>
2018-12-13 18:47:41 -0500

    Init commit

---
+++ dist/server.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+const port = 4040;
+app_1.default.listen(port, function () {
+    console.log('Express server listening on port ' + port);
+    console.log('Inho le boss');
+});
+//# sourceMappingURL=server.js.map
